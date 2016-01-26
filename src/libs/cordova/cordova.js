6bb65de586f1c46d99f9567639263c1d23746438
Tim Haase <timhaase@gmx.net>
2016-01-26 22:34:33 +0100

    Fix typo in cordova import

--- src/libs/cordova/cordova.js
+++ src/libs/cordova/cordova.js
@@ -1,4 +1,4 @@
-var cordva = window.cordova;
+var cordova = window.cordova;
 // Set the __esModule flag. This lets babel return the exports object
 // directly instead of writing it into the 'default' property of the
 // returned object.
